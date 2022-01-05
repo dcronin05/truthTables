cb9f5171312074922bc4d9047aade4ee8f70b673
Daniel Cronin <daniel@dcron.in>
2022-01-04 22:41:05 -0700

    Weird recursion error.

--- main.py
+++ main.py
@@ -5,30 +5,48 @@ from tabulate import tabulate
 
 class TruthTable:
 
-    table = {}
+    def __init__(self, num_var):
+        self.table = {}
+        self.create_table(self, num_var)
 
-    def __init__(self, var_count=2):
-        self.create_table(var_count)
-
-    def create_table(var_count, var_1='p', var_2='q', var_3='r', var_4='', var_5='', var_6=''):
+    @staticmethod
+    def create_table(self, var_count, var_1='p', var_2='q', var_3='r', var_4='', var_5='', var_6=''):
         true = 'T'
         false = 'F'
 
+        two_var_table = {var_1: [true, true, false, false],
+                         var_2: [true, false, true, false]}
+
+        three_var_table = {var_1: [true, true, true, true, false, false, false, false],
+                           var_2: [true, true, false, false, true, true, false, false],
+                           var_3: [true, true, true, true, false, false, false, false]}
+
         if var_count == 2:
-            table = {var_1: [true, true, false, false],
-                     var_2: [true, false, true, false]
-                     }
+            self.set_table(two_var_table)
 
         if var_count == 3:
-            table = {var_1: [true, true, true, true, false, false, false, false],
-                     var_2: [true, true, false, false, true, true, false, false],
-                     var_3: [true, false, true, false, true, false, true, false]
-                     }
+            self.set_table(three_var_table)
 
-        return pd.DataFrame(table)
+    def add_column(self, header, equation):
+
+
+    def set_table(self, value):
+        self.table = value
+
+    def get_table(self):
+        return self.get_table()
+
+    def disp_table(self):
+        print(self.get_table())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(tabulate(TruthTable.create_table(3, 'p', 'q', 'r'), headers='keys', tablefmt='github', showindex=False))
-    print(tabulate(TruthTable.create_table(2, 'p', 'q'), headers='keys', tablefmt='github', showindex=False))
+    table_1 = TruthTable(2)
+    table_2 = TruthTable(3)
+
+    print(tabulate(table_1.get_table(), headers='keys', tablefmt='github', showindex=False))
+
+    print()
+
+    print(tabulate(table_2.get_table(), headers='keys', tablefmt='github', showindex=False))
